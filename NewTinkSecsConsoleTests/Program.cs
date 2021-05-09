0eb1b46f4ceb0dca7c1714ca3da5456998759774
Artem Pereshein <pereshein.artem@gmail.com>
2021-05-09 15:41:19 +0300

    Added console test project.

---
+++ NewTinkSecsConsoleTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewTinkSecsConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

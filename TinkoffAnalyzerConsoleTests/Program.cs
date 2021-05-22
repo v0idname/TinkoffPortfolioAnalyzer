8867bd988f1a65266c9792eba6fd36aaaa94e795
Artem Pereshein <pereshein.artem@gmail.com>
2021-05-22 20:01:37 +0300

    Исправлены предупреждения компилятора.

--- TinkoffAnalyzerConsoleTests/Program.cs
+++ TinkoffAnalyzerConsoleTests/Program.cs
@@ -15,7 +15,7 @@ namespace NewTinkSecsConsoleTests
         private static async Task GetAllBonds()
         {
             // токен аутентификации
-            var sandboxToken = "t.qnjT83GCBu8TWdiXRcuqCnUXoh_Z-sstY0eyhq9duzZ8uKqZfOzLeculB8A3lou_Rl7pfMF6k8wvIp-6kNVBxQ";
+            //var sandboxToken = "t.qnjT83GCBu8TWdiXRcuqCnUXoh_Z-sstY0eyhq9duzZ8uKqZfOzLeculB8A3lou_Rl7pfMF6k8wvIp-6kNVBxQ";
             var tradingToken = "t.zbKUmxP9SRmY14raZfJewpclgi0It5QO53eSoAalFM8uuTN82d--xg8H-swG61jd0z0lx2F9-0B8kGYLKhWzxw";
 
             // для работы в песочнице используйте GetSandboxConnection

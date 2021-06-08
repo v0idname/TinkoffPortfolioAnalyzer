5609cbe746cd1e746c01873a3ac20108674c203a
Artem Pereshein <pereshein.artem@gmail.com>
2021-06-08 22:38:50 +0300

    Необходимое для вкладки "доступные бумаги" вынесено в отдельный класс.

---
+++ TinkoffPortfolioAnalyzer/ViewModels/AvailSecuritiesViewModel.cs
@@ -0,0 +1,80 @@
+﻿using Library.Commands;
+using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Tinkoff.Trading.OpenApi.Models;
+using TinkoffPortfolioAnalyzer.Models;
+
+namespace TinkoffPortfolioAnalyzer.ViewModels
+{
+    internal class AvailSecuritiesViewModel
+    {
+        //TODO: ObservableCollection ??
+        public List<AvailSecSnapshot> AvailSecSnapshots { get; }
+
+        public List<AvailSecSnapshot> SelectedAvailSecSnapshots { get; set; }
+
+        public ICommand CreateSnapshotCommand { get; }
+
+        private bool CanCreateSnapshotCommandExecute(object p) => true;
+
+        private void OnCreateSnapshotCommandExecuted(object p)
+        {
+            //var newSnapshot = new AvailSecSnapshot
+            //{
+            //    CreatedDateTime = DateTime.Now,
+            //    Securities = new List<SecurityInfo>()
+            //};
+
+            //var bonds = _curConnectContext.MarketBondsAsync().GetAwaiter().GetResult();
+            //foreach (var bond in bonds.Instruments)
+            //{
+            //    newSnapshot.Securities.Add(new SecurityInfo
+            //    {
+            //        Name = bond.Name,
+            //        Ticker = bond.Ticker,
+            //        InstrumentType = bond.Type,
+            //        Currency = bond.Currency
+            //    });
+            //}
+            //AvailSecSnapshots.Add(newSnapshot);
+        }
+
+        public ICommand SelectedSnapChangedCommand { get; }
+
+        private bool CanSelectedSnapChangedCommandExecute(object p) => true;
+
+        private void OnSelectedSnapChangedCommandExecuted(object p)
+        {
+            ;
+        }
+
+        public AvailSecuritiesViewModel()
+        {
+            AvailSecSnapshots = new List<AvailSecSnapshot>();
+            for (int snapIndex = 0; snapIndex < 3; snapIndex++)
+            {
+                var newSnapshot = new AvailSecSnapshot
+                {
+                    CreatedDateTime = new DateTime(DateTime.Now.Ticks).AddDays(snapIndex),
+                    Securities = new List<SecurityInfo>()
+                };
+                for (int i = 0; i < 10 + snapIndex; i++)
+                {
+                    newSnapshot.Securities.Add(new SecurityInfo
+                    {
+                        Name = $"Security name {i}",
+                        Ticker = $"Security ticker {i}",
+                        InstrumentType = InstrumentType.Bond,
+                        Currency = Currency.Rub
+                    });
+                }
+                AvailSecSnapshots.Add(newSnapshot);
+            }
+
+            CreateSnapshotCommand = new RelayCommand(OnCreateSnapshotCommandExecuted, CanCreateSnapshotCommandExecute);
+            SelectedSnapChangedCommand = new RelayCommand(OnSelectedSnapChangedCommandExecuted, CanSelectedSnapChangedCommandExecute);
+            SelectedAvailSecSnapshots = new List<AvailSecSnapshot>();
+        }
+    }
+}
--- TinkoffPortfolioAnalyzer/ViewModels/MainWindowViewModel.cs
+++ TinkoffPortfolioAnalyzer/ViewModels/MainWindowViewModel.cs
@@ -7,12 +7,9 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Data;
-using System.Windows.Input;
-using Library.Commands;
 using Tinkoff.Trading.OpenApi.Models;
 using Tinkoff.Trading.OpenApi.Network;
 using TinkoffPortfolioAnalyzer.Models;
-using System.Collections.ObjectModel;
 
 namespace TinkoffPortfolioAnalyzer.ViewModels
 {
@@ -109,49 +106,6 @@ namespace TinkoffPortfolioAnalyzer.ViewModels
         }
         #endregion
 
-        #region AvailSecSnapshots
-
-        //TODO: ObservableCollection ??
-        public List<AvailSecSnapshot> AvailSecSnapshots { get; }
-
-        public List<AvailSecSnapshot> SelectedAvailSecSnapshots { get; set; }
-
-        public ICommand CreateSnapshotCommand { get; }
-
-        private bool CanCreateSnapshotCommandExecute(object p) => true;
-
-        private void OnCreateSnapshotCommandExecuted(object p)
-        {
-            //var newSnapshot = new AvailSecSnapshot
-            //{
-            //    CreatedDateTime = DateTime.Now,
-            //    Securities = new List<SecurityInfo>()
-            //};
-
-            //var bonds = _curConnectContext.MarketBondsAsync().GetAwaiter().GetResult();
-            //foreach (var bond in bonds.Instruments)
-            //{
-            //    newSnapshot.Securities.Add(new SecurityInfo
-            //    {
-            //        Name = bond.Name,
-            //        Ticker = bond.Ticker,
-            //        InstrumentType = bond.Type,
-            //        Currency = bond.Currency
-            //    });
-            //}
-            //AvailSecSnapshots.Add(newSnapshot);
-        }
-
-        public ICommand SelectedSnapChangedCommand { get; }
-
-        private bool CanSelectedSnapChangedCommandExecute(object p) => true;
-
-        private void OnSelectedSnapChangedCommandExecuted(object p)
-        {
-            ;
-        }
-        #endregion
-
         public CollectionViewSource SecuritiesViewSource { get; } = new CollectionViewSource();
 
         public MainWindowViewModel()
@@ -169,31 +123,6 @@ namespace TinkoffPortfolioAnalyzer.ViewModels
                     });
             }
             TinkoffTokens = tinkTokens;
-
-            AvailSecSnapshots = new List<AvailSecSnapshot>();
-            for (int snapIndex = 0; snapIndex < 3; snapIndex++)
-            {
-                var newSnapshot = new AvailSecSnapshot
-                {
-                    CreatedDateTime = new DateTime(DateTime.Now.Ticks).AddDays(snapIndex),
-                    Securities = new List<SecurityInfo>()
-                };
-                for (int i = 0; i < 10 + snapIndex; i++)
-                {
-                    newSnapshot.Securities.Add(new SecurityInfo
-                    {
-                        Name = $"Security name {i}",
-                        Ticker = $"Security ticker {i}",
-                        InstrumentType = InstrumentType.Bond,
-                        Currency = Currency.Rub
-                    });
-                }
-                AvailSecSnapshots.Add(newSnapshot);
-            }
-            
-            CreateSnapshotCommand = new RelayCommand(OnCreateSnapshotCommandExecuted, CanCreateSnapshotCommandExecute);
-            SelectedSnapChangedCommand = new RelayCommand(OnSelectedSnapChangedCommandExecuted, CanSelectedSnapChangedCommandExecute);
-            SelectedAvailSecSnapshots = new List<AvailSecSnapshot>();
         }
 
         private async Task<IEnumerable<TinkoffAccount>> GetAccounts(TinkoffToken token)

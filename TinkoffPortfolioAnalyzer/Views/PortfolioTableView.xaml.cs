4b707a7c692cea52a468500417ca7a847856c8b6
Artem Pereshein <pereshein.artem@gmail.com>
2021-09-23 22:41:00 +0300

    Содержимое вкладок разнесено по разным view.

---
+++ TinkoffPortfolioAnalyzer/Views/PortfolioTableView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TinkoffPortfolioAnalyzer.Views
+{
+    /// <summary>
+    /// Interaction logic for PortfolioTableView.xaml
+    /// </summary>
+    public partial class PortfolioTableView : UserControl
+    {
+        public PortfolioTableView()
+        {
+            InitializeComponent();
+        }
+    }
+}

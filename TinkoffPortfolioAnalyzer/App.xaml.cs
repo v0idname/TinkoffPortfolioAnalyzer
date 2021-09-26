28288d947aacab4fb3945ca098b33253723b62ad
Artem Pereshein <pereshein.artem@gmail.com>
2021-09-26 21:10:28 +0300

    Добавлен хост для реализации инверсии зависимости.

--- TinkoffPortfolioAnalyzer/App.xaml.cs
+++ TinkoffPortfolioAnalyzer/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
+using TinkoffPortfolioAnalyzer.Services;
+using TinkoffPortfolioAnalyzer.ViewModels;
 
 namespace TinkoffPortfolioAnalyzer
 {
@@ -7,5 +12,29 @@ namespace TinkoffPortfolioAnalyzer
     /// </summary>
     public partial class App : Application
     {
+        private static IHost __Host;
+
+        public static IHost Host => __Host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var host = Host;
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+        }
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<DataService>();
+            services.AddSingleton<MainWindowViewModel>();
+        }
     }
 }
---
+++ TinkoffPortfolioAnalyzer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace TinkoffPortfolioAnalyzer
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            App app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        internal static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host_builder = Host.CreateDefaultBuilder(args);
+            host_builder.UseContentRoot(Environment.CurrentDirectory);
+            host_builder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            });
+            host_builder.ConfigureServices(App.ConfigureServices);
+            return host_builder;
+        }
+    }
+}

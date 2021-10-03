84c8a3e4d37b711a1bdb298a1b03a5093f5fa7ff
Artem Pereshein <pereshein.artem@gmail.com>
2021-10-03 21:21:22 +0300

    SelectedItemsBehavior вынесен в отдельный файл.

--- TinkoffPortfolioAnalyzer/ViewModels/SelectedItems.cs
+++ TinkoffPortfolioAnalyzer/ViewModels/SelectedItems.cs
@@ -43,27 +43,4 @@ namespace TinkoffPortfolioAnalyzer.ViewModels
             return behavior;
         }
     }
-
-    public class SelectedItemsBehavior
-    {
-        private readonly ListBox _listBox;
-        private readonly IList _boundList;
-
-        public SelectedItemsBehavior(ListBox listBox, IList boundList)
-        {
-            _boundList = boundList;
-            _listBox = listBox;
-            _listBox.SelectionChanged += OnSelectionChanged;
-        }
-
-        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            _boundList.Clear();
-
-            foreach (var item in _listBox.SelectedItems)
-            {
-                _boundList.Add(item);
-            }
-        }
-    }
 }
---
+++ TinkoffPortfolioAnalyzer/ViewModels/SelectedItemsBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Windows.Controls;
+
+namespace TinkoffPortfolioAnalyzer.ViewModels
+{
+    public class SelectedItemsBehavior
+    {
+        private readonly ListBox _listBox;
+        private readonly IList _boundList;
+
+        public SelectedItemsBehavior(ListBox listBox, IList boundList)
+        {
+            _boundList = boundList;
+            _listBox = listBox;
+            _listBox.SelectionChanged += OnSelectionChanged;
+        }
+
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _boundList.Clear();
+
+            foreach (var item in _listBox.SelectedItems)
+            {
+                _boundList.Add(item);
+            }
+        }
+    }
+}

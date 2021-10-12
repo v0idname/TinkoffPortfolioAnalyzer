16b2bc7ee69d5dc75c01196e7c6f2c2a7773e67f
Artem Pereshein <pereshein.artem@gmail.com>
2021-10-13 00:14:28 +0300

    Удалены отладочные проекты.

--- TinkoffAnalyzerConsoleTests/Program.cs
+++ TinkoffAnalyzerConsoleTests/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace NewTinkSecsConsoleTests
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using TinkoffPortfolioAnalyzer.Data;
+
+namespace NewTinkSecsConsoleTests
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.ReadLine();
         }
     }
 }
